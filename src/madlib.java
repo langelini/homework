e5511f42e0cf6454e3e62bd5e29c0e7867cd0066
langelini <leif_angelini27@milton.edu>
2024-11-20 10:25:15 -0500

    madlib

--- src/madlib.java
+++ src/madlib.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class madlib {
 
     String classmate1;
@@ -10,22 +12,27 @@ public class madlib {
     int number2;
     int number3;
 
+
     public static void main(String[] args) {
+
         madlib mystory = new madlib("jack", "john", "boston", "cool", "dog", 1, 20, 50);
-        System.out.println(mystory);
-    }
-    public madlib(String sclassmate1, String sclassmate2, String splace, String sadjective, String spet, int snumber1, int snumber2, int snumber3 ){
-        String story = "this weekend i'm going to " + sclassmate1 + " house. afterwards i'm going with "+sclassmate2+" to "+splace+". it's going to be so " + sadjective + ". "+ sclassmate2 + " has a " + spet + ". on a scale of "+snumber1+" to " + snumber2+ " it was about a " + snumber3;
-        classmate1= sclassmate1;
-        classmate2 = sclassmate2;
-        place = splace;
-        adjective = sadjective;
-        pet = spet;
-        number1 = snumber1;
-        number2 = snumber2;
-        number3 = snumber3;
-
 
     }
+
+    public madlib(String sclassmate1, String sclassmate2, String splace, String sadjective, String spet, int snumber1,
+        int snumber2, int snumber3 ){
+            String story = "this weekend i'm going to " + sclassmate1 + " house. afterwards i'm going with " + sclassmate2 + " to " + splace + ". it's going to be so " + sadjective + ". " + sclassmate2 + " has a " + spet + ". on a scale of " + snumber1 + " to " + snumber2 + " it was about a " + snumber3+".";
+            classmate1 = sclassmate1;
+            classmate2 = sclassmate2;
+            place = splace;
+            adjective = sadjective;
+            pet = spet;
+            number1 = snumber1;
+            number2 = snumber2;
+            number3 = snumber3;
+        System.out.println(story);
+
+
+        }
 }
 
